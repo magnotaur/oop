33d24b118eaa332a4ec7c1fc0b88c4cdcbc985ff
magnotaur <magnotaur@gmail.com>
2019-05-25 17:48:19 +0500

    класс point

---
+++ Oop/Oop/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);   //вывод на экран
+            Console.Write(sym);
+        }
+    }
+}
