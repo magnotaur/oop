dabd6fb34c6c9f64ffa73eea423c600c3c3c47aa
magnotaur <magnotaur@gmail.com>
2019-05-25 17:24:07 +0500

    Функция Draw

--- Oop/Oop/Program.cs
+++ Oop/Oop/Program.cs
@@ -10,8 +10,26 @@ namespace Oop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            int x1 = 1;         // точка + с координатами 1 и 3
+            int y1 = 3;
+            char sym1 = '+';
+
+            Draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
+            
+
             Console.ReadLine();
         }
+
+        static void Draw(int x, int y, char sym)    //функция Draw, чтобы избавиться от постоянного копирования вывода на экран каждой точки
     }
+    {
+            Console.SetCursorPosition(x, y);   //вывод на экран
+            Console.Write(sym);
+}
 }
