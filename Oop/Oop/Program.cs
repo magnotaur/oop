5a4145747e2b9c286001794e4afe35f0bf4cc5eb
magnotaur <magnotaur@gmail.com>
2019-05-26 19:56:16 +0500

    горизонтальная линия

--- Oop/Oop/Program.cs
+++ Oop/Oop/Program.cs
@@ -10,11 +10,34 @@ namespace Oop
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
+            Point p1 = new Point(1, 3, '+');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
+
+           /* List<int> numList = new list<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.Remove(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2); */
 
             Console.ReadLine();
         }
