4f9b94164e2fb1ae8783e99f7b11b4e8fadbf3f5
magnotaur <magnotaur@gmail.com>
2019-05-26 22:01:16 +0500

    Наследование. Созал класс Figure. Перенес туда код, который повторяется в HorizontalLine и verticalLine

--- Oop/Oop/Program.cs
+++ Oop/Oop/Program.cs
@@ -10,36 +10,49 @@ namespace Oop
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '+');
-            p1.Draw();
+            /* Point p1 = new Point(1, 3, '+');
+             p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+             Point p2 = new Point(4, 5, '#');
+             p2.Draw(); */
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
+            Console.SetBufferSize(80, 25);
 
-           /* List<int> numList = new list<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            //Отрисовка рамочки
+            HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
+            upline.Drow();
+            downline.Drow();
+            leftline.Drow();
+            rightline.Drow();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
+            // Отричовка точек
+            Point p = new Point(4, 5, '*');
+            p.Draw();
 
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
+            /* List<int> numList = new list<int>();
+             numList.Add(0);
+             numList.Add(1);
+             numList.Add(2);
 
-            numList.Remove(0);
+             int x = numList[0];
+             int y = numList[1];
+             int z = numList[2];
 
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2); */
+             foreach (int i in numList)
+             {
+                 Console.WriteLine(i);
+             }
 
-            Console.ReadLine();
+             numList.Remove(0);
+
+             List<Point> pList = new List<Point>();
+             pList.Add(p1);
+             pList.Add(p2); */
+
+            Console.ReadLine(); 
         }
     }
 }
--- Oop/Oop/VerticalLine.cs
+++ Oop/Oop/VerticalLine.cs
@@ -6,18 +6,17 @@ using System.Threading.Tasks;
 
 namespace Oop
 {
-    class HorizontalLine : Figure
+    class VerticalLine : Figure
     {
-     
-        public HorizontalLine(int xLeft, int xReight, int y, char sym)
+        public VerticalLine(int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
-            for (int x = xLeft; x <= xReight; x++)
+            for (int y = yUp; y <= yDown; y++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
-            
+
         }
     }
 }
